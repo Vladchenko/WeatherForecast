abd6cc930513d9b5cf9ae3a9f164b221667902cc
Vlad <vladdasaev@gmail.com>
2026-02-20 22:27:59 +0300

    refactor: remaining obsolete exceptions removed

--- app/src/main/java/com/example/weatherforecast/data/api/customexceptions/CityNotFoundException.kt
+++
@@ -1,9 +0,0 @@
-package com.example.weatherforecast.data.api.customexceptions
-
-/**
- * Exception to mark the state when specific city was not found on server.
- *
- * @property city city name
- * @property message custom message
- */
-data class CityNotFoundException(val city: String, override val message:String): Exception(message)
--- app/src/main/java/com/example/weatherforecast/data/api/customexceptions/Exceptions.kt
+++ app/src/main/java/com/example/weatherforecast/data/api/customexceptions/Exceptions.kt
@@ -1,39 +1,11 @@
 package com.example.weatherforecast.data.api.customexceptions
 
 /**
- * Exception raised during communication with the remote api. Contains http [statusCode].
- *
- * @property statusCode to define the reason of problem.
- */
-data class ApiException(val statusCode: String) : Exception()
-
-/**
- * Exception indicating that device is not connected to the internet.
- *
- * @param [message].
- */
-class NoInternetException(message: String) : Exception(message)
-
-/**
- * Exception indicating network timeout occurred.
- *
- * @param message error message
- */
-class NetworkTimeoutException(message: String) : Exception(message)
-
-/**
  * Exception indicating there is no such entry
  *
  * @param entryName name of the database entry that is absent.
  */
 class NoSuchDatabaseEntryException(entryName: String) : Exception(entryName)
-
-/**
- * Not handled unexpected exception.
- *
- * @param cause of the exception.
- */
-class UnexpectedException(cause: Exception) : Exception(cause)
 
 /**
  * Exception indicating geo location is not available.
